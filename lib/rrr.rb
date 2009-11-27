4448bc23666df25eddf1614e1aea282299f51bd9
maiha <maiha@wota.jp>
2009-11-27 22:36:53 +0900

    Refactored

--- lib/rrr.rb
+++ lib/rrr.rb
@@ -5,46 +5,14 @@ require 'blankslate'
 
 module RRR
   extend RR::Adapters::RRMethods
-
-  class Proxy < BlankSlate
-    def initialize(klass)
-      @klass = klass
-    end
-  end
-
-  class ClassProxy < Proxy
-    private
-      def method_missing(*args, &block)
-        RRR.stub(@klass).__send__(*args, &block)
-      end
-  end
-
-  class InstanceProxy < Proxy
-    private
-      def method_missing(*args, &block)
-        RRR.mock.instance_of(@klass).__send__(*args, &block)
-      end
-  end
-
-  def self.included(klass)
-    klass.class_eval do
-      include RR::Adapters::RRMethods
-      klass.extend ClassMethods
-    end
-  end
-
-  module ClassMethods
-    def cm(&block)
-      ClassProxy.new(self).__send__(:instance_eval, &block)
-    end
-
-    def im(&block)
-      InstanceProxy.new(self).__send__(:instance_eval, &block)
-    end
-  end
 end
 
-class Object
-  include RRR
-end
+class << Object
+  def cm(&block)
+    RRR.stub(self).__send__(:instance_eval, &block)
+  end
 
+  def im(&block)
+    RRR.stub.instance_of(self).__send__(:instance_eval, &block)
+  end
+end
--- spec/im_spec.rb
+++ spec/im_spec.rb
@@ -2,8 +2,56 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe "RRR" do
   describe "im" do
-    it "TODO" do
-      pending "This file should be filled with cm_spec"
+    before do
+      Object.send(:remove_const, 'Foo') rescue nil
+      class Foo
+        im {
+          bar      {'bar'}
+
+          foo(1)   {'foo1'}
+          foo('x') {'foox'}
+        }
+      end
+    end
+
+    context "when no arged method is defined" do
+      it "adds class methods" do
+        Foo.new.should respond_to(:bar)
+      end
+
+      it "allows empty args" do
+        Foo.new.bar.should == 'bar'
+      end
+
+      it "ignores args" do
+        Foo.new.bar(1).should == 'bar'
+      end
+    end
+
+    context "when only immediate integer or string is defined" do
+      it "adds class methods" do
+        Foo.new.should respond_to(:foo)
+      end
+
+      it "should raise an error against empty args" do
+        lambda {
+          Foo.new.foo
+        }.should raise_error(RR::Errors::DoubleNotFoundError)
+      end
+
+      it "allows method overload for immediate integer" do
+        Foo.new.foo(1).should == 'foo1'
+      end
+
+      it "allows method overload for immediate string" do
+        Foo.new.foo('x').should == 'foox'
+      end
+
+      it "raise an error against symbol" do
+        lambda {
+          Foo.new.foo(:x)
+        }.should raise_error(RR::Errors::DoubleNotFoundError)
+      end
     end
   end
 end
